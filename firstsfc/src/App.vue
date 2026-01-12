35673dde3ab746ca92a07dcbeb3cd4329ad179d5
apcflencomienda <flencomienda@student.apc.edu.ph>
2026-01-12 15:26:54 +0800

    vue test 2

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -3,7 +3,7 @@
 <template>
   <h1>Boker Tov Manila, Shalom Makati</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to know the stuff
     documentation
   </p>
 </template>
