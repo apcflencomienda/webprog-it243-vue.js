50ce940f10829ccec8e03014b832d49d545213f2
apcflencomienda <flencomienda@student.apc.edu.ph>
2026-01-12 15:25:54 +0800

    vue test 1

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>You da Program Cuh Yung, U the yung Stunna!</h1>
+  <h1>Boker Tov Manila, Shalom Makati</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
