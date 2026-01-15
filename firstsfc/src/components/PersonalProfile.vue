799941bac00c3e2b512ba33a9f9960a6a2d71c68
apcflencomienda <flencomienda@student.apc.edu.ph>
2026-01-15 15:13:35 +0800

    correction 4: added personal vue

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,38 +1,53 @@
 <template>
-<body>
   <div class="container">
-    <header>  About Me  
-      <br>About Me Full name: Francis Frederick L. Encomienda (Flencomienda)
-
+    <header>
+      About Me <br>
+      About Me Full name: Francis Frederick L. Encomienda (Flencomienda)
     </header>
+
     <main>
       <section class="main-content">
-        <div>1 Goals In Life / Dream
-        <br>Goals In Life / Dream
-        <br> Giving life a chance
+        <div>
+          1 Goals In Life / Dream <br>
+          Goals In Life / Dream <br>
+          Giving life a chance
         </div>
-        
-        <div>2Education / Achievements
-        <br>Education / Achievements
-      <br>• Completed Senior High School in ABM strand (2021), Asia Pacific College
-      <br>• Accounting, Business Management Specializing in Management
- 
-        </div>
-        <div>3 Course </div>
-        <div>4 IT Experience
-      <br>IT Experience
-      <br>Work Experience
 
+        <div>
+          2 Education / Achievements <br>
+          Education / Achievements <br>
+          • Completed Senior High School in ABM strand (2021), Asia Pacific College <br>
+          • Accounting, Business Management Specializing in Management
+        </div>
+
+        <div>3 Course</div>
+
+        <div>
+          4 IT Experience <br>
+          IT Experience <br>
+          Work Experience
         </div>
       </section>
-       <aside class="sidebar">
-        Hobbies & Interests
-      <br> Military Simulator Games
-      <br> Playing slow paced games
-        </aside>
+
+      <aside class="sidebar">
+        Hobbies & Interests <br>
+        Military Simulator Games <br>
+        Playing slow paced games
+      </aside>
     </main>
+
     <footer>
-        Picture Gallery
+      Picture Gallery
     </footer>
   </div>
-</body>
+</template>
+
+<script>
+export default {
+  // You can add component logic here later
+}
+</script>
+
+<style>
+/* You can add your CSS styles here later */
+</style>
