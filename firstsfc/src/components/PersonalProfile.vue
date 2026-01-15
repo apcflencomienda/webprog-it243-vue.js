f07aa62997b8d4a6d73477e9824daf9b4fa8f433
apcflencomienda <flencomienda@student.apc.edu.ph>
2026-01-15 15:10:39 +0800

    3rd component addition

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,38 @@
+<template>
+<body>
+  <div class="container">
+    <header>  About Me  
+      <br>About Me Full name: Francis Frederick L. Encomienda (Flencomienda)
+
+    </header>
+    <main>
+      <section class="main-content">
+        <div>1 Goals In Life / Dream
+        <br>Goals In Life / Dream
+        <br> Giving life a chance
+        </div>
+        
+        <div>2Education / Achievements
+        <br>Education / Achievements
+      <br>• Completed Senior High School in ABM strand (2021), Asia Pacific College
+      <br>• Accounting, Business Management Specializing in Management
+ 
+        </div>
+        <div>3 Course </div>
+        <div>4 IT Experience
+      <br>IT Experience
+      <br>Work Experience
+
+        </div>
+      </section>
+       <aside class="sidebar">
+        Hobbies & Interests
+      <br> Military Simulator Games
+      <br> Playing slow paced games
+        </aside>
+    </main>
+    <footer>
+        Picture Gallery
+    </footer>
+  </div>
+</body>
